bc0474a870cb9ce78e860905de9810cab8cc0ab2
ketamin333 <fomin.casch@yandex.ru>
2026-04-29 17:32:45 +0300

    added redis

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,24 +3,26 @@ package config
 import "os"
 
 type Config struct {
-	ServerPort string
-	DBHost     string
-	DBPort     string
-	DBUser     string
-	DBPassword string
-	DBName     string
-	DBSSLMode  string
+	ServerPort    string
+	DBHost        string
+	DBPort        string
+	DBUser        string
+	DBPassword    string
+	DBName        string
+	DBSSLMode     string
+	RedisPassword string
 }
 
 func New() *Config {
 	return &Config{
-		ServerPort: env("SERVER_PORT", "8080"),
-		DBHost:     env("DB_HOST", ""),
-		DBPort:     env("DB_PORT", ""),
-		DBUser:     env("DB_USER", ""),
-		DBPassword: env("DB_PASSWORD", ""),
-		DBName:     env("DB_NAME", ""),
-		DBSSLMode:  env("DB_SSL_MODE", ""),
+		ServerPort:    env("SERVER_PORT", "8080"),
+		DBHost:        env("DB_HOST", ""),
+		DBPort:        env("DB_PORT", ""),
+		DBUser:        env("DB_USER", ""),
+		DBPassword:    env("DB_PASSWORD", ""),
+		DBName:        env("DB_NAME", ""),
+		DBSSLMode:     env("DB_SSL_MODE", ""),
+		RedisPassword: env("REDIS_PASSWORD", ""),
 	}
 }
 
